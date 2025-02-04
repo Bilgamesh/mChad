af96f65e4366590b02f016cca9e0829b79e2a64f
Bilgamesh <24696613+Bilgamesh@users.noreply.github.com>
2025-02-04 21:32:34 +0100

    fix: potential bugs when re-rendering chat

--- www/views/app/index.js
+++ www/views/app/index.js
@@ -120,10 +120,7 @@
         documentUtil
       });
 
-      globalSynchronizer.addSyncListener('refresh-end', async () => {
-        await sleep(500);
-        badges.refreshBadges();
-      });
+      globalSynchronizer.addSyncListener('refresh-end', badges.refreshBadges);
 
       const router = Router({
         Errors,
